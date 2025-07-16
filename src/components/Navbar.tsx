54d61b27e6d3bad6aff2d98e2af9371e29943745
Mark <brandonnarciso24@gmail.com>
2025-07-15 22:32:19 -0700

    feat: Add links to my icons and redirect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,34 @@
 import mangaIcon from "../assets/img/manga-icon.png";
 import shoppingCartIcon from "../assets/img/shopping-cart-icon.png";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="flex m-4 md:m-10 items-center justify-between md:self-start">
-      <img src={mangaIcon} alt="manga book icon" height="auto" width={60} />
+      <Link to="/">
+        <img
+          src={mangaIcon}
+          alt="manga book icon"
+          height="auto"
+          width={60}
+          className="transition delay-100 duration-300 ease-in-out hover:scale-120 cursor-pointer"
+        />
+      </Link>
       {/* Change this to Searchbar */}
       <input
-        className="border-2 rounded-full px-2 md:w-[300px]"
+        className="border-1 rounded-lg px-2 md:w-[300px] h-[35px]"
         type="text"
         placeholder="Search Manga..."
       />
-      <img
-        src={shoppingCartIcon}
-        alt="shopping cart"
-        height="auto"
-        width={60}
-      />
+      <Link to="/shop">
+        <img
+          src={shoppingCartIcon}
+          alt="shopping cart"
+          height="auto"
+          width={60}
+          className="transition delay-100 duration-300 ease-in-out hover:scale-120 cursor-pointer"
+        />
+      </Link>
     </div>
   );
 }
