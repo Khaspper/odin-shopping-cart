ffc3f0d8853f0e586394d884183c7bd4db2db5b8
Mark <brandonnarciso24@gmail.com>
2025-07-17 06:26:35 -0700

    feat: finish designing the manga page

--- src/components/MangaPage.tsx
+++ src/components/MangaPage.tsx
@@ -9,29 +9,32 @@ export default function MangaPage() {
   )!;
   //TODO: change this design for the navbar
   return (
-    <div className="h-full bg-zinc-800 flex flex-col gap-5 items-center px-3 py-5">
-      <Navbar />
-      <picture>
-        <source srcSet={chosenManga.imgWEBP} type="image/webp" />
-        <img
-          src={chosenManga.imgJPG}
-          alt={`Cover manga image for ${chosenManga.titleEnglish}`}
-          width={300}
-          crossOrigin="anonymous"
-        />
-      </picture>
-      <section className="flex flex-col text-white gap-2">
-        <p className="text-white p-2 overflow-auto bg-zinc-700 rounded-2xl text-center text-5xl flex flex-col gap-2">
-          {chosenManga.titleEnglish}
-          <br />
+    <div className="h-full lg:h-screen bg-zinc-800 flex flex-col gap-5 items-center px-3 py-5 lg:grid lg:grid-cols-2 ">
+      <div className="grid">
+        <div>
+          <Navbar />
+        </div>
+        <picture className="flex justify-center">
+          <source srcSet={chosenManga.imgWEBP} type="image/webp" />
+          <img
+            src={chosenManga.imgJPG}
+            alt={`Cover manga image for ${chosenManga.titleEnglish}`}
+            width={300}
+            crossOrigin="anonymous"
+          />
+        </picture>
+      </div>
+      <section className="flex flex-col text-white gap-5">
+        <div className="text-white p-2 overflow-auto bg-zinc-700 rounded-2xl text-center text-5xl flex flex-col gap-2">
+          <p>{chosenManga.titleEnglish}</p>
           <p className="text-base">{chosenManga.titleJapanese}</p>
-        </p>
+        </div>
         <section className="text-white rounded-2xl grid grid-cols-2 gap-2">
           <section className="bg-zinc-700 p-2 rounded-2xl">
             <p>Authors:</p>
             <div className="flex justify-center flex-col overflow-auto gap-1">
               {chosenManga.authors.map((author) => (
-                <p>{author}</p>
+                <p key={author}>{author}</p>
               ))}
             </div>
           </section>
@@ -45,16 +48,16 @@ export default function MangaPage() {
         </p>
         <ul className="text-white p-2 max-h-[100px] overflow-auto bg-zinc-700 rounded-2xl list-disc list-inside">
           {chosenManga.genre.map((genre) => (
-            <li>{genre}</li>
+            <li key={genre}>{genre}</li>
           ))}
         </ul>
+        <button
+          type="button"
+          className="bg-zinc-700 container py-5 rounded-full text-white hover:cursor-pointer transition delay-100 duration-300 ease-in-out hover:scale-103 ml-auto mr-auto"
+        >
+          Add to cart
+        </button>
       </section>
-      <button
-        type="button"
-        className="bg-zinc-700 container py-5 rounded-full text-white hover:cursor-pointer transition delay-100 duration-300 ease-in-out hover:scale-103"
-      >
-        Add to cart
-      </button>
     </div>
   );
 }
