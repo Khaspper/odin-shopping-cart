dd002ead7bd780d5b52e1e602b9ece9f426481ef
Mark <brandonnarciso24@gmail.com>
2025-07-16 20:46:35 -0700

    fix: UI

--- src/components/MangaCard.tsx
+++ src/components/MangaCard.tsx
@@ -8,15 +8,16 @@ type TMangaCard = {
 export default function MangaCard({ manga }: TMangaCard) {
   console.log(manga);
   return (
-    <Link to={`test/${manga.id}`}>
-      <div className="flex flex-col justify-between grow w-[90px] md:w-[100px] xl:w-[110px] p-3">
-        <img
-          src={manga.imgJPG}
-          alt={`${manga.titleEnglish} manga cover`}
-          className="transition delay-100 duration-300 ease-in-out hover:scale-110 cursor-pointer"
-        />
-        <p className="truncate text-center text-white">{manga.titleEnglish}</p>
-      </div>
+    <Link
+      to={`test/${manga.id}`}
+      className="h-full flex flex-col justify-between grow shrink min-w-[60px] max-w-[90px] md:w-[100px] xl:w-[110px] p-3"
+    >
+      <img
+        src={manga.imgJPG}
+        alt={`${manga.titleEnglish} manga cover`}
+        className="transition delay-100 duration-300 ease-in-out hover:scale-110 cursor-pointer"
+      />
+      <p className="truncate text-center text-white">{manga.titleEnglish}</p>
     </Link>
   );
 }
--- src/components/PersonalFavorites.tsx
+++ src/components/PersonalFavorites.tsx
@@ -3,9 +3,9 @@ import { favoriteMangas } from "../assets/FavoriteManga";
 
 export default function PersonalFavorites() {
   return (
-    <div className="flex m-4 md:m-10 self-end gap-3.5 bg-black/50 backdrop-blur-sm rounded-xl flex-col w-fit h-[min(50vw,200px)] items-center md:w-[max(500px,40vw)] md:h-[230px]">
+    <div className="flex m-4 md:m-10 self-end gap-3.5 bg-black/50 backdrop-blur-sm rounded-xl flex-col w-fit h-[min(50vw,200px)] items-center md:w-[max(450px,25vw)] md:h-[200px]">
       <p className="text-white text-center p-1">My Favorite Manga</p>
-      <div className="flex flex-wrap truncate">
+      <div className="flex flex-wrap truncate justify-center items-">
         {favoriteMangas.map((manga) => (
           <MangaCard key={manga.id} manga={manga} />
         ))}
