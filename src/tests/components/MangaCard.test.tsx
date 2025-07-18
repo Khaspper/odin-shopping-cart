a355562036cdff89146d5db60e3e5a1f930b3756
Mark <brandonnarciso24@gmail.com>
2025-07-17 18:22:53 -0700

    fix: move test folder

--- src/tests/components/MangaCard.test.tsx
+++ src/tests/components/MangaCard.test.tsx
@@ -1,31 +1,36 @@
-import React from "react";
 import { describe, it, expect } from "vitest";
 import { render, screen, within } from "@testing-library/react";
-import MangaCard from "../../src/components/MangaCard";
+import MangaCard from "../../components/MangaCard";
 import { MemoryRouter } from "react-router-dom";
-import { favoriteMangas } from "../../src/components/data/MangaData";
+import { favoriteMangas } from "../../components/data/MangaData";
 import "@testing-library/jest-dom";
+import { beforeAll } from "vitest";
 
 const manga = favoriteMangas[0];
 
 describe("MangaCard Component", () => {
-  it("renders correctly", () => {
+  let link: HTMLElement;
+  let image: HTMLElement;
+  beforeAll(() => {
     render(
       <MemoryRouter>
         <MangaCard manga={manga} />
       </MemoryRouter>
     );
+    link = screen.getByRole("link");
+    image = screen.getByRole("img", {
+      name: `${manga.titleEnglish} manga cover`,
+    });
+  });
+
+  it("renders correctly", () => {
     //? Link renders
-    const link = screen.getByRole("link");
     expect(link).toHaveAttribute("href", `/manga/${manga.titleEnglish}`);
 
     //? Title renders
     expect(within(link).getByText(manga.titleEnglish)).toBeInTheDocument();
 
     //? Image renders
-    const image = screen.getByRole("img", {
-      name: `${manga.titleEnglish} manga cover`,
-    });
     expect(image).toBeInTheDocument();
     expect(image).toHaveAttribute("src", manga.imgJPG);
   });
